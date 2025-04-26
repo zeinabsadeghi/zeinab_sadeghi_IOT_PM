10aed476fa118b1408c784bef695baa4b893f62f
zeinabsadeghi <zeinabsad711364@gmail.com>
2025-04-26 07:10:01 +0330

    Update Non_Attend_Finalproject1.py

--- Non_Attend_Finalproject1.py
+++ Non_Attend_Finalproject1.py
@@ -925,149 +925,251 @@ mygroups[1].turn_on()
 
 #==========================================================
 
-class AdminPanel:
-
+#----------------------nemoneye akahri
+class admin_panel():
     def __init__(self):
         self.groups={}
         
+        
     def create_group(self,group_name):
-        ''' give me the name for one section of your house'''
         
         if group_name not in self.groups:
             self.groups[group_name]=[]
-            print(f'groups {group_name} created')
-            #logg mimone
-        else:
-            print('yout group name is duplicated name')
+            print(f'group {group_name} is created')
             
+        else:
+            print('your name is dublicated')
+        
+        
     def add_device_to_group(self,group_name,device):
+        
         if group_name in self.groups:
             self.groups[group_name].append(device)
             
-           
+
         else:
-            print(f'Group {group_name} does not exist')
-        
-        
-    #dota function ro neveshtam ta inja bvrsm
-    
-    
+            print('your group is not created')
+            
+            
+            #d1=Device('home/group/device_type/name')
+            
     def create_device(self,group_name,device_type,name):
         
         if group_name in self.groups:
             topic=f'home/{group_name}/{device_type}/{name}'
             new_device=Device(topic)
             self.add_device_to_group(group_name, new_device)
-            
+            #F'DEVICE IS CREATE'
+           print(f'device {device.name} is created')
             
         else:
-            print(f'Group {group_name} does not exist')
+            print('your group is not created')
             
             
-    def create_multiple_devices(self,group_name,device_type,number_of_devices):
+            
+    def create_multiple_devices(self,group_name,device_type,number_of_devcies):
+        
         if group_name in self.groups:
-            for i in range(1,number_of_devices+1):
-                
-                device_name=f"{device_type}{i}"
-                
-                topic=f'home/{group_name}/{device_type}/{device_name.lower()}'
             
+            for i in range(1,number_of_devcies+1):
+                #number=10 -> 1,2,3,4,5,6,7,8,9,10 -->i
+                
+                device_name=f'{device_type}{i}'
+                
+                topic=f'home/{group_name}/{device_type}/{device_name}'
+                
                 new_device=Device(topic)
                 
                 self.add_device_to_group(group_name, new_device)
-            
+                print(f'device {new_device.name} created')
+
             
         else:
-            print(f'Group {group_name} does not exist')
+            print('your group is not created')
             
             
             
-            
-        
     def get_devices_in_groups(self,group_name):
         if group_name in self.groups:
-            return self.groups[group_name]
-         
+            return self.groups[group_name] #listi az device
+            
+            
         else:
-            print(f'Group {group_name} does not exist')
+            print('your group is not created')
             return []
+            
+            
+            #tamame device haye yek grooh ro roshan koen
+    def turn_on_all_in_groups(self,group_name):
         
+        devices=self.get_devices_in_groups(group_name)
+        #list 
+        #{'living_room'}:[d1,d2,d3,df34,....]
+        #devices=[d1,d2,d3,df34,....]
         
-
-    def turn_on_all_in_group(self,group_name):
-        devices=self.devices_ingroups(group_name)
         for device in devices:
-            device.turn_one()
+            #DEVICE() class
+            #in class --> .location .name .group .device__type
+            #.turn_on turn_off
+            device.turn_on()
+            print(f'{device.name} in group {group_name} is turned ON')
             
             
             
-    def turn_off_all_in_group(self,group_name):
+    def turn_off_all_in_groups(self,group_name):
+        '''
+        yek group name begire mesle living_room
+        tamame device haro khamosh kone
         
-        ''' hameye cheragh haye yek goroh ra khamosh konad'''
         
-        pass
+        '''
+        device= self.get_devices_in_groups(group_name)
+        for device in devices:
+            device.turn_off()
+            print(f'{device.name} in group {group_name} is turned OFF')
+            
+
+    
+    
+
+    def turn_on_all_devices(self):
+        '''
+        tamam device haro roshan kone
+        **na fghht vase ye goroh
+        '''            
+        for group_name, device in self.group.items():
+            for device in devices:
+                device.turn_on
+                print(f'{device.name} in group {group_name} is turn ON')
+    
+    def turn_off_all_devices(self):
+        '''
+        tamam device haro khamoosh kone
+        **na fghht vase ye goroh
+        '''            
+        for group_name, device in self.group.items():
+            for device in devices:
+                device.turn_off
+                print(f'{device.name} in group {group_name} is turn OFF')
+    
+    def get_status_in_group(self,gorup_name):
         
-    def trun_on_all(self):
-        '''hameye device haro roshan kone'''
-        pass
+        '''
         
-    def turn_off_all(self):
-        '''hameye devcie haro khamosh kone'''
-        pass
+        group_name bdi
+        bege device ha done done roshanan ya kahmoosh
         
-    def get_status_in_group(self,group_name):
+        print kone
+        device {esme} --> on
+        devcie {} --> off
         
-        '''living_room y matn print mikone mige lamp1 on , klamp2 off , lamp3 ,..'''
-        pass
+        
+        '''
+    
+        device=self.get_devices_in_groups(gorup_name)
+        for device in devices:
+            status=device.get_status()
+            print(f'device {device.name} --> {status}')
+    
+    
     def get_status_in_device_type(self,device_type):
         
-        ''' device=lamps --> tamame lamp haro status mohem nabashe tooye living rome kojas'''
-        pass
-    def create_sensor(self) :
-    #bar asase clASS SENSOR argument bzarid
-        pass
+        '''
+        device_type-->masalan lamp
+        
+        name , too kodom goroh on off
+        
+        lamp2 in living_room is off
+        lamp3 in wc is on
+        
+        '''
+        for group_name,devices in self.groups.items():
+            for device in devices:
+                if device.device_type==device_type:
+                    status=device.get_status()
+                print(f'{device.name} in {group_name} is {status}')
+        
+    #Sensor --> a1=Sensor(...)
+    #def create_device
+    
+    def create_sensor(self,group_name,sensor_type,name):
+        if group_name in self.groups:
+            topic=f'home/{group_name}/{sensor_type}/{name}'
+            new_sensor=sensor(topic)
+            self.groups[group_name].append(new_sensor)
+            print(f'sensor {new_sensor.name} is created in group')
+        else:
+            print('your group is nat created')
+    
+    def add_sensor_in_group(self,group_name,sensor):
+        #yek group name bede va to sensor ro tooye oon list ezaf koni
+        
+        #add_devcie_in_group
+        
+        if group_name in self.groups:
+            self.groups[group_name].append(sensor)
+            print(f'sensor {sensor.name} added to group {group_name}')
+
+        else:
+            print('your group is not added')
     
     
-    
-    def get_status_sensor_in_group(self,group_name):
+    def get_data_from_sensor_in_group(self,group_name):
+        if group_name in self.groups:
+            for device in self.groups[group_name]:
+                if isinstance(device,sensor):
+                    data=device.get_data()
+                    print(f'{device.name} --> {data}')
+
+        else:
+            print("your group don't have data")
         
         '''
         
-        sensor haye yek goroh ro biad doone dooen status ro pas bde
-        
+        lkiving__room --> tamame sensor haro mire behet 
+        datasho pas mide
         '''
-        pass
         
+    def summarize_group_activity(self, group_name):
+    """
+    اطلاعات خلاصه‌ی یک گروه:
+    - تعداد کل دستگاه‌ها و تعداد روشن‌ها
+    - تعداد سنسورها و میانگین داده‌ها
+    """
+    if group_name not in self.groups:
+        print("Group does not exist.")
+        return
+    
+    devices = self.groups[group_name]
+    device_count = 0
+    device_on_count = 0
+    sensor_count = 0
+    sensor_data_list = []
 
-        
+    for item in devices:
+        if isinstance(item, Device):
+            device_count += 1
+            if item.get_status().lower() == 'on':
+                device_on_count += 1
+        elif isinstance(item, Sensor):
+            sensor_count += 1
+            try:
+                data = float(item.get_data())
+                sensor_data_list.append(data)
+            except:
+                continue # Ignore sensors with non-numeric data
 
+    print(f"\nSummary for group '{group_name}':")
+    print(f"Devices: {device_count} total, {device_on_count} ON")
+    print(f"Sensors: {sensor_count} total")
+    if sensor_data_list:
+        avg_data = sum(sensor_data_list) / len(sensor_data_list)
+        print(f"Average Sensor Data: {avg_data:.2f}")
+    else:
+        print("No numeric sensor data available.")
 
+    
+    
 
-a1=AdminPanel()
-
-a1.create_group('living_room')
-a1.create_multiple_devices('living_room','lamps',40)
-
-a1.turn_on_all_in_group('living_room')
-
-#check koni bbini roshan 
-
-mygroups=a1.groups['living_room']
-
-mygroups[1].name #lamps2'
-
-mygroups[1].turn_on()
-
-        
-
-#besazi
-        
-
-
-
-
-
-
-
-
-
+            
+            
